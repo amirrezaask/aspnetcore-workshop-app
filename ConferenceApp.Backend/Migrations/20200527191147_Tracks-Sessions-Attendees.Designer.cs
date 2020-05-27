c07ad8f2035060dd4386f2955b0f8bac2db616e8
Amirrezaask <raskarpour@gmail.com>
2020-05-27 23:42:02 +0430

    new migrations

--- ConferenceApp.Backend/Migrations/20200527191147_Tracks-Sessions-Attendees.Designer.cs
+++ ConferenceApp.Backend/Migrations/20200527191147_Tracks-Sessions-Attendees.Designer.cs
@@ -4,14 +4,16 @@ using ConferenceApp.Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Backend.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200527191147_Tracks-Sessions-Attendees")]
+    partial class TracksSessionsAttendees
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
