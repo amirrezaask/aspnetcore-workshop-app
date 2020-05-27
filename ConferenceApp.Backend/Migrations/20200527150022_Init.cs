4ddc634bbd27f0fb12433d9ead16464ac95dcc80
Amirrezaask <raskarpour@gmail.com>
2020-05-27 19:31:21 +0430

    Init migration

---
+++ ConferenceApp.Backend/Migrations/20200527150022_Init.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Backend.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Speakers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Bio = table.Column<string>(maxLength: 4000, nullable: true),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    Website = table.Column<string>(maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speakers", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Speakers");
+        }
+    }
+}
