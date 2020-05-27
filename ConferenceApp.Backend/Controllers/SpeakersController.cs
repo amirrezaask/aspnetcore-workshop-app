8bdf14e527483e27b898310eb03dddd4c959695f
Amirrezaask <raskarpour@gmail.com>
2020-05-27 23:52:34 +0430

    Refactor SpeakerController, remove unused APIs, refactor GetSpeaker method

--- ConferenceApp.Backend/Controllers/SpeakersController.cs
+++ ConferenceApp.Backend/Controllers/SpeakersController.cs
@@ -35,72 +35,16 @@ namespace Backend
         [HttpGet("{id}")]
         public async Task<ActionResult<Speaker>> GetSpeaker(int id)
         {
-            var speaker = await _context.Speakers.FindAsync(id);
+            var speaker = await _context.Speakers
+                .AsNoTracking()
+                .Include(s => s.Sessions)
+                .ThenInclude(ss => ss.Session)
+                .SingleOrDefaultAsync();
 
             if (speaker == null)
             {
                 return NotFound();
             }
-
-            return speaker;
-        }
-
-        // PUT: api/Speakers/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutSpeaker(int id, Speaker speaker)
-        {
-            if (id != speaker.ID)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(speaker).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SpeakerExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Speakers
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<Speaker>> PostSpeaker(Speaker speaker)
-        {
-            _context.Speakers.Add(speaker);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetSpeaker", new { id = speaker.ID }, speaker);
-        }
-
-        // DELETE: api/Speakers/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Speaker>> DeleteSpeaker(int id)
-        {
-            var speaker = await _context.Speakers.FindAsync(id);
-            if (speaker == null)
-            {
-                return NotFound();
-            }
-
-            _context.Speakers.Remove(speaker);
-            await _context.SaveChangesAsync();
 
             return speaker;
         }
