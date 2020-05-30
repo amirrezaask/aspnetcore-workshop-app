ec4ed653b43948c7afa322ca03a11b6cd9c3cb14
amirrezaask <raskarpour@gmail.com>
2020-05-30 17:00:36 +0430

    Data seeders

--- ConferenceApp.Backend/Data/DatabaseContext.cs
+++ ConferenceApp.Backend/Data/DatabaseContext.cs
@@ -11,6 +11,26 @@ namespace ConferenceApp.Backend.Data {
         {
             modelBuilder.Entity<SessionAttendee>().HasKey(sa => new { sa.AttendeeId, sa.SessionId });
             modelBuilder.Entity<SessionSpeaker>().HasKey(ss => new { ss.SpeakerId, ss.SessionId });
+
+            // seeding data
+            modelBuilder.Entity<Speaker>().HasData(new Speaker
+            {
+                ID = 1,
+                Name = "Scott hanselman",
+                Bio = "Awesome Scott",
+                Website = "hanselman.com",
+            });
+            modelBuilder.Entity<Session>().HasData(new Session
+            {
+                ID = 1,
+                Title = "Keynote",
+            });
+            modelBuilder.Entity<SessionSpeaker>().HasData(new SessionSpeaker 
+            {
+                SpeakerId = 1,
+                SessionId = 1,
+            });
+
         }
         public DbSet<Speaker> Speakers {get; set; }
         public DbSet<Session> Sessions { get; set; }
